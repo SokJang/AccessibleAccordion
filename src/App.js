69cac668adaba475950e26fc5e504b56cb5585a7
Suk-Be Jang <suk-be.jang@dw.com>
2020-04-09 08:57:47 +0200

    basic useState for accordion header, click and enter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 import { Accordion } from "./components/Accordion";
 import { AccordionSection } from "./components/AccordionSection";
 
-const App = () => (
-  <div className={styles.Wrapper}>
-    <Accordion>
-      <AccordionSection title="section 1">content 1</AccordionSection>
-      <AccordionSection title="section 2" expanded>
-        <a href="#test">Alice </a>
-        was beginning to get very tired of sitting by her sister on the bank,
-        and of having nothing to do: once or twice she had peeped into the book
-        her sister was reading, but it had no pictures or conversations in it,
-        “and what is the use of a book,” thought Alice “without pictures or
-        conversations?”
-      </AccordionSection>
-    </Accordion>
-  </div>
-);
-
+const App = () => {
+  const [expanded1, setExpanded1] = useState(false);
+  return (
+    <div className={styles.Wrapper}>
+      <Accordion>
+        <AccordionSection
+          title="section 1"
+          expanded={expanded1}
+          onToggle={() => setExpanded1(!expanded1)}
+        >
+          content 1
+        </AccordionSection>
+        <AccordionSection title="section 2" expanded>
+          <a href="#test">Alice </a>
+          was beginning to get very tired of sitting by her sister on the bank,
+          and of having nothing to do: once or twice she had peeped into the
+          book her sister was reading, but it had no pictures or conversations
+          in it, “and what is the use of a book,” thought Alice “without
+          pictures or conversations?”
+        </AccordionSection>
+      </Accordion>
+    </div>
+  );
+};
 export default App;
